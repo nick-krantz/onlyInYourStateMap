466dff9fe61c92a563bf8920f201defc277ff23f
krantni <krantni@gmail.com>
2019-06-04 22:13:42 -0500

    Think I'm sending too many requests at a time

--- app/app.ts
+++ app/app.ts
@@ -12,18 +12,49 @@ for (let i = 2; i<=30; i++){
   promises.push(axios.get(`${url}page/${i}/`))
 }
 
-console.log('Waiting for promises to resolve...')
+console.log('Waiting for promises to resolve...');
+
+const reflect = (promise: Promise<AxiosResponse<any>>) => {
+  return promise.then(v => {
+    return {status: 'success', data: v.data}
+  }).catch(() => {
+    return {status: 'error', data: null}
+  });
+}
 
 Promise.all(promises).then(responses => {
-  console.log('Received Web Data... ')
+  console.log('Received All Web Data... ');
   responses.map(response => {
-    const $ = cheerio.load(response.data)
+    const $ = cheerio.load(response.data);
     $('article a').each((index, link)=>{
       index;
       links.push({title: link.attribs.title, url: link.attribs.href});
     });
   });
-  console.log(links.length);
+  console.log('Starting Web Scrape of Individual Pages... ');
+  const pagePromises: Promise<AxiosResponse<any>>[] = [];
+  links.map((link, i) => {
+    if(i > 45){
+      return;
+    }
+    pagePromises.push(axios.get(link.url));
+  });
+  console.log('Waiting for Individual Pages to resolve... ');
+  Promise.all(pagePromises.map(reflect)).then(pageResponses =>{
+    console.log('Received each page... ');
+    const successfulPromises = pageResponses.filter(pageResponse => pageResponse.status === 'success')
+    successfulPromises.map(page =>{
+        const $ = cheerio.load(page.data);
+        const figCaptions = $('figure figcaption');
+        const captionText = figCaptions.first().text();
+        const textArray = captionText.split('Address: ');
+        if(textArray.length > 1) {
+          console.log(textArray[1]);
+      }
+    });
+  }).catch(err => {
+    console.log(err);
+  });
 }).catch(error => {
   console.error(error);
 })
--- build/app.js
+++ build/app.js
@@ -13,8 +13,15 @@ for (var i = 2; i <= 30; i++) {
     promises.push(axios_1.default.get(url + "page/" + i + "/"));
 }
 console.log('Waiting for promises to resolve...');
+var reflect = function (promise) {
+    return promise.then(function (v) {
+        return { status: 'success', data: v.data };
+    }).catch(function () {
+        return { status: 'error', data: null };
+    });
+};
 Promise.all(promises).then(function (responses) {
-    console.log('Received Web Data... ');
+    console.log('Received All Web Data... ');
     responses.map(function (response) {
         var $ = cheerio_1.default.load(response.data);
         $('article a').each(function (index, link) {
@@ -22,7 +29,30 @@ Promise.all(promises).then(function (responses) {
             links.push({ title: link.attribs.title, url: link.attribs.href });
         });
     });
-    console.log(links.length);
+    console.log('Starting Web Scrape of Individual Pages... ');
+    var pagePromises = [];
+    links.map(function (link, i) {
+        if (i > 45) {
+            return;
+        }
+        pagePromises.push(axios_1.default.get(link.url));
+    });
+    console.log('Waiting for Individual Pages to resolve... ');
+    Promise.all(pagePromises.map(reflect)).then(function (pageResponses) {
+        console.log('Received each page... ');
+        var successfulPromises = pageResponses.filter(function (pageResponse) { return pageResponse.status === 'success'; });
+        successfulPromises.map(function (page) {
+            var $ = cheerio_1.default.load(page.data);
+            var figCaptions = $('figure figcaption');
+            var captionText = figCaptions.first().text();
+            var textArray = captionText.split('Address: ');
+            if (textArray.length > 1) {
+                console.log(textArray[1]);
+            }
+        });
+    }).catch(function (err) {
+        console.log(err);
+    });
 }).catch(function (error) {
     console.error(error);
 });
