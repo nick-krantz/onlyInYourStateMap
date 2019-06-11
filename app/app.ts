b7078ee3f8d25b51ad93487be4d64cbb2007a0a8
krantni <krantni@gmail.com>
2019-06-10 20:37:10 -0500

    this is ugly but getting closer - I'll fix it

--- app/app.ts
+++ app/app.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosResponse } from 'axios';
 import cheerio from 'cheerio';
+import fs from 'fs';
 
 console.log('Starting Web Scrape...')
 
@@ -7,6 +8,8 @@ const url = 'https://www.onlyinyourstate.com/states/minnesota/';
 const links:Link[] = [];
 
 const promises: Promise<AxiosResponse<any>>[] = [axios.get(url)];
+
+const finalArray:{name: string, url: string, address: string}[] = [];
 
 for (let i = 2; i<=30; i++){
   promises.push(axios.get(`${url}page/${i}/`))
@@ -16,8 +19,10 @@ console.log('Waiting for promises to resolve...');
 
 const reflect = (promise: Promise<AxiosResponse<any>>) => {
   return promise.then(v => {
+    console.log("success!");
     return {status: 'success', data: v.data}
   }).catch(() => {
+    console.log("error!");
     return {status: 'error', data: null}
   });
 }
@@ -33,24 +38,23 @@ Promise.all(promises).then(responses => {
   });
   console.log('Starting Web Scrape of Individual Pages... ');
   const pagePromises: Promise<AxiosResponse<any>>[] = [];
-  links.map((link, i) => {
-    if(i > 45){
-      return;
-    }
-    pagePromises.push(axios.get(link.url));
+  links.map((link) => {
+    pagePromises.push(axios.get(link.url, {timeout: 60000}));
   });
   console.log('Waiting for Individual Pages to resolve... ');
+  let noAddress = 0;
+  let noURL = 0;
 
   Promise.all(pagePromises.map(reflect)).then(pageResponses =>{
     console.log('Received each page... ');
     const successfulPromises = pageResponses.filter(pageResponse => pageResponse.status === 'success')
     successfulPromises.map(page =>{
         const $ = cheerio.load(page.data);
-        const figCaptions = $('figure figcaption');
-        const captionText = figCaptions.first().text();
-        const textArray = captionText.split('Address: ');
+        let elementsThatContainAddress = $("*:contains(\"Address: \")");
+        let captionText = elementsThatContainAddress.text();
+        let textArray = captionText.split('Address: ');
         if(textArray.length > 1) {
-          console.log(textArray[1]);
+          console.log(textArray[textArray.length-1].split(".")[0])
           const url = $('meta[property="og:url"]').attr('content');
           console.log(url);
           let name = ""
@@ -60,9 +64,76 @@ Promise.all(promises).then(responses => {
             const names = name.split('-');
             name = names.map(thing => {return thing.charAt(0).toUpperCase() + thing.slice(1)}).join(" ");
             console.log(name);
+            finalArray.push({
+              name,
+              url,
+              address: textArray[textArray.length-1].split(".")[0]
+            })
+          } else {
+            noURL++;
           }
+        } else {
+          elementsThatContainAddress = $("*:contains(\"You\'ll find it at: \")");
+          captionText = elementsThatContainAddress.text();
+          textArray = captionText.split('You\'ll find it at: ');
+          if(textArray.length > 1) {
+            console.log(textArray[1].split(".\n")[0])
+            const url = $('meta[property="og:url"]').attr('content');
+            console.log(url);
+            let name = ""
+            if(url.split('/minnesota/').length > 1){
+              name = url.split('/minnesota/')[1];
+              name = name.slice(0,-3);
+              const names = name.split('-');
+              name = names.map(thing => {return thing.charAt(0).toUpperCase() + thing.slice(1)}).join(" ");
+              console.log(name);
+              finalArray.push({
+                name,
+                url,
+                address: textArray[1].split(". ")[0]
+              })
+            } else {
+              noURL++;
+            }
+        } else {
+          elementsThatContainAddress = $("*:contains(\"is located at \")");
+          captionText = elementsThatContainAddress.text();
+          textArray = captionText.split('is located at ');
+          if(textArray.length > 1) {
+            console.log(textArray[1].split(". ")[0])
+            const url = $('meta[property="og:url"]').attr('content');
+            console.log(url);
+            let name = ""
+            if(url.split('/minnesota/').length > 1){
+              name = url.split('/minnesota/')[1];
+              name = name.slice(0,-3);
+              const names = name.split('-');
+              name = names.map(thing => {return thing.charAt(0).toUpperCase() + thing.slice(1)}).join(" ");
+              console.log(name);
+              finalArray.push({
+                name,
+                url,
+                address: textArray[1].split(". ")[0]
+              })
+            } else {
+              noURL++;
+            }
+        } else {
+          noAddress++;
         }
+      }
+    }
     });
+    console.log("No url found: ", noURL);
+    console.log("No Address found: ", noAddress);
+    console.log("Final Array Count", finalArray.length);
+    const serializedArray = JSON.stringify(finalArray);
+    fs.writeFile('./output.txt', serializedArray, (err) => {
+      if(err){
+        return console.log(err)
+      }
+      console.log("File created!")
+    })
   }).catch(err => {
     console.log(err);
   });
--- build/app.js
+++ build/app.js
@@ -5,18 +5,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var axios_1 = __importDefault(require("axios"));
 var cheerio_1 = __importDefault(require("cheerio"));
+var fs_1 = __importDefault(require("fs"));
 console.log('Starting Web Scrape...');
 var url = 'https://www.onlyinyourstate.com/states/minnesota/';
 var links = [];
 var promises = [axios_1.default.get(url)];
+var finalArray = [];
 for (var i = 2; i <= 30; i++) {
     promises.push(axios_1.default.get(url + "page/" + i + "/"));
 }
 console.log('Waiting for promises to resolve...');
 var reflect = function (promise) {
     return promise.then(function (v) {
+        console.log("success!");
         return { status: 'success', data: v.data };
     }).catch(function () {
+        console.log("error!");
         return { status: 'error', data: null };
     });
 };
@@ -31,23 +35,22 @@ Promise.all(promises).then(function (responses) {
     });
     console.log('Starting Web Scrape of Individual Pages... ');
     var pagePromises = [];
-    links.map(function (link, i) {
-        if (i > 45) {
-            return;
-        }
-        pagePromises.push(axios_1.default.get(link.url));
+    links.map(function (link) {
+        pagePromises.push(axios_1.default.get(link.url, { timeout: 60000 }));
     });
     console.log('Waiting for Individual Pages to resolve... ');
+    var noAddress = 0;
+    var noURL = 0;
     Promise.all(pagePromises.map(reflect)).then(function (pageResponses) {
         console.log('Received each page... ');
         var successfulPromises = pageResponses.filter(function (pageResponse) { return pageResponse.status === 'success'; });
         successfulPromises.map(function (page) {
             var $ = cheerio_1.default.load(page.data);
-            var figCaptions = $('figure figcaption');
-            var captionText = figCaptions.first().text();
+            var elementsThatContainAddress = $("*:contains(\"Address: \")");
+            var captionText = elementsThatContainAddress.text();
             var textArray = captionText.split('Address: ');
             if (textArray.length > 1) {
-                console.log(textArray[1]);
+                console.log(textArray[textArray.length - 1].split(".")[0]);
                 var url_1 = $('meta[property="og:url"]').attr('content');
                 console.log(url_1);
                 var name_1 = "";
@@ -57,8 +60,81 @@ Promise.all(promises).then(function (responses) {
                     var names = name_1.split('-');
                     name_1 = names.map(function (thing) { return thing.charAt(0).toUpperCase() + thing.slice(1); }).join(" ");
                     console.log(name_1);
+                    finalArray.push({
+                        name: name_1,
+                        url: url_1,
+                        address: textArray[textArray.length - 1].split(".")[0]
+                    });
+                }
+                else {
+                    noURL++;
                 }
             }
+            else {
+                elementsThatContainAddress = $("*:contains(\"You\'ll find it at: \")");
+                captionText = elementsThatContainAddress.text();
+                textArray = captionText.split('You\'ll find it at: ');
+                if (textArray.length > 1) {
+                    console.log(textArray[1].split(". ")[0]);
+                    var url_2 = $('meta[property="og:url"]').attr('content');
+                    console.log(url_2);
+                    var name_2 = "";
+                    if (url_2.split('/minnesota/').length > 1) {
+                        name_2 = url_2.split('/minnesota/')[1];
+                        name_2 = name_2.slice(0, -3);
+                        var names = name_2.split('-');
+                        name_2 = names.map(function (thing) { return thing.charAt(0).toUpperCase() + thing.slice(1); }).join(" ");
+                        console.log(name_2);
+                        finalArray.push({
+                            name: name_2,
+                            url: url_2,
+                            address: textArray[1].split(". ")[0]
+                        });
+                    }
+                    else {
+                        noURL++;
+                    }
+                }
+                else {
+                    elementsThatContainAddress = $("*:contains(\"is located at \")");
+                    captionText = elementsThatContainAddress.text();
+                    textArray = captionText.split('is located at ');
+                    if (textArray.length > 1) {
+                        console.log(textArray[1].split(". ")[0]);
+                        var url_3 = $('meta[property="og:url"]').attr('content');
+                        console.log(url_3);
+                        var name_3 = "";
+                        if (url_3.split('/minnesota/').length > 1) {
+                            name_3 = url_3.split('/minnesota/')[1];
+                            name_3 = name_3.slice(0, -3);
+                            var names = name_3.split('-');
+                            name_3 = names.map(function (thing) { return thing.charAt(0).toUpperCase() + thing.slice(1); }).join(" ");
+                            console.log(name_3);
+                            finalArray.push({
+                                name: name_3,
+                                url: url_3,
+                                address: textArray[1].split(". ")[0]
+                            });
+                        }
+                        else {
+                            noURL++;
+                        }
+                    }
+                    else {
+                        noAddress++;
+                    }
+                }
+            }
+        });
+        console.log("No url found: ", noURL);
+        console.log("No Address found: ", noAddress);
+        console.log("Final Array Count", finalArray.length);
+        var serializedArray = JSON.stringify(finalArray);
+        fs_1.default.writeFile('./output.txt', serializedArray, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            console.log("File created!");
         });
     }).catch(function (err) {
         console.log(err);
