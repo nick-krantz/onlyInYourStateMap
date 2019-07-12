51aa690686e6422bdaeebca025a15e24dc6523ae
krantni <krantni@gmail.com>
2019-07-11 21:22:48 -0500

    resolving locations via HERE api

--- app/app.ts
+++ app/app.ts
@@ -1,3 +1,4 @@
+import { AxiosResponse } from 'axios';
 import fs from "fs";
 import { appCode, appId } from "./api";
 import axios from './axios';
@@ -11,27 +12,51 @@ fs.readFile("./output.txt", "utf8", (err, data) => {
   getGeoInformation(parsedData);
 });
 
+const promises: Promise<AxiosResponse<any>>[] = [];
+const addresses: HereLocation[] = [];
+
 const getGeoInformation = (parsedData: Location[]) => {
 
   parsedData.forEach(location => {
 // tslint:disable-next-line: max-line-length
     const hereMapsURL = `https://geocoder.api.here.com/6.2/geocode.json?app_id=${appId}&app_code=${appCode}&searchtext=${location.address}`;
-    axios.get(hereMapsURL).then(response => {
+    promises.push(axios.get(hereMapsURL).then(res =>({...res, Name: location.name, Url: location.url})));
+  });
+
+  Promise.all(promises).then(responses => {
+    responses.forEach(response => {
       const hereLocation = transformResponse(response);
+      if (hereLocation) {
+        addresses.push(hereLocation);
+      }
     });
+    console.log(addresses);
   });
 };
 
 const transformResponse = (res: any) => {
-  if(res.data.Response.View.length && res.data.Response.View[0].Result.length){
-    return res.data.Response.View[0].Result.Location;
+  if (res.data.Response.View.length && res.data.Response.View[0].Result.length) {
+    return {...res.data.Response.View[0].Result[0].Location.Address, Name: res.Name, Url: res.Url};
   }
   return null;
-}
+};
 
 
 interface Location {
   address: string;
   name: string;
   url: string;
+}
+
+interface HereLocation {
+  Name: string;
+  Url: string;
+  Label: string;
+  Country: string;
+  State: string;
+  County: string;
+  City: string;
+  Street: string;
+  HouseNumber: string;
+  PostalCode: string;
 }
--- build/app.js
+++ build/app.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -13,12 +24,27 @@ fs_1.default.readFile("./output.txt", "utf8", function (err, data) {
     var parsedData = JSON.parse(data);
     getGeoInformation(parsedData);
 });
+var promises = [];
+var addresses = [];
 var getGeoInformation = function (parsedData) {
     parsedData.forEach(function (location) {
         // tslint:disable-next-line: max-line-length
         var hereMapsURL = "https://geocoder.api.here.com/6.2/geocode.json?app_id=" + api_1.appId + "&app_code=" + api_1.appCode + "&searchtext=" + location.address;
-        axios_1.default.get(hereMapsURL).then(function (response) {
-            console.log(response);
-        });
+        promises.push(axios_1.default.get(hereMapsURL).then(function (res) { return (__assign({}, res, { Name: location.name, Url: location.url })); }));
     });
+    Promise.all(promises).then(function (responses) {
+        responses.forEach(function (response) {
+            var hereLocation = transformResponse(response);
+            if (hereLocation) {
+                addresses.push(hereLocation);
+            }
+        });
+        console.log(addresses);
+    });
+};
+var transformResponse = function (res) {
+    if (res.data.Response.View.length && res.data.Response.View[0].Result.length) {
+        return __assign({}, res.data.Response.View[0].Result[0].Location.Address, { Name: res.Name, Url: res.Url });
+    }
+    return null;
 };
